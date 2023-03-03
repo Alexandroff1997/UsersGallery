2258124836dff319712ff9bdcb32484852760c40
Alex <alexandroff1997@gmail.com>
2023-03-04 02:06:31 +0700

    implementation of logic for creating and getting users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { UserCreateDto } from './dto/user-create.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+	constructor(private usersService: UsersService) {}
+
+	@Post()
+	create(@Body() dto: UserCreateDto) {
+		return this.usersService.createUser(dto);
+	}
+
+	@Get()
+	getAllUsers() {
+		return this.usersService.getAllUsers();
+	}
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { UserCreateDto } from './dto/user-create.dto';
+import { UsersModel } from './users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+	constructor(@InjectModel(UsersModel) private userRepository: typeof UsersModel) {}
+
+	async createUser(dto: UserCreateDto) {
+		const user = await this.userRepository.create(dto);
+		return user;
+	}
+
+	async getAllUsers() {
+		const users = await this.userRepository.findAll();
+		return users;
+	}
+}
